34ea89c31eefe7bc84beb3f2078b2c482ace6568
marcelo-corpucci <mcorpucci@velocitypartners.net>
2016-02-02 18:01:54 -0300

    Port added to main.py importing from os

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from app import app, db
 import admin
 import api
@@ -7,5 +9,7 @@ import views
 from entries.blueprint import entries
 app.register_blueprint(entries, url_prefix='/entries')
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=port)
